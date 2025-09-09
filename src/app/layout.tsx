40ae4fdb60d0a33c9fd96e1f8fc97b1d5e087865
HimanshuKumarDutt094 <adasimobenio@gmail.com>
2025-09-09 13:31:40 +0530

    chore:fix build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,9 +58,7 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${geist.variable}`}>
-      <body>
-        <>{children}</>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
